414c4b4474f566759363bf99af76b944eecab8c7
Fahadk85 <60100766+Fahadk85@users.noreply.github.com>
2023-07-13 13:20:39 -0400

    Fixed mistake in html file Added functionality to reset button

--- index.js
+++ index.js
@@ -30,7 +30,17 @@ pauseBtn.addEventListener("click", () => {
     }
 });
 
-resetBtn.addEventListener();
+resetBtn.addEventListener("click",() => {
+    paused = true;
+    clearInterval(intervalId);
+    startTime = 0;
+    elapsedTime = 0;
+    currentTime = 0;
+    hrs = 0;
+    mins = 0;
+    secs = 0;
+    timeDisplay.textContent = "00:00:00";
+});
 
 
 function updateTime(){
